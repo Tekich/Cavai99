38de6195102730a6ea0674f13dbc3b00a056304a
cavai9 <cavai9@mail.ru>
2018-05-11 17:08:24 +0300

    (*-*)

---
+++ Site1/scripts/main.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/background.jpg') {
+      myImage.setAttribute ('src','images/background2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/background.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML  = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML  = 'Good day for die, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
